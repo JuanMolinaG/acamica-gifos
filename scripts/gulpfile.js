b638cda9d75160f732361d170944e7540c7a526b
Juan G. Molina <jg.molina92@hotmail.com>
2020-02-27 12:36:04 -0500

    Add assets folder, css sourcemaps, first css styles and components

--- scripts/gulpfile.js
+++ scripts/gulpfile.js
@@ -1,12 +1,15 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
 var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('sass', function () {
     return gulp.src('../src/scss/**/*.scss')
-      .pipe(sass().on('error', sass.logError))
-      .pipe(autoprefixer())
-      .pipe(gulp.dest('../public/css'));
+        .pipe(sourcemaps.init())
+        .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+        .pipe(autoprefixer())
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('../public/css'));
 });
 
 gulp.task('default', function () {
