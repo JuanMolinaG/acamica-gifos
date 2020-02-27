7ea2e4055293cdb10ac7a1084bcef601b0355aea
Juan G. Molina <jg.molina92@hotmail.com>
2020-02-26 19:20:16 -0500

    Move gulpfile to scripts directory

--- scripts/gulpfile.js
+++ scripts/gulpfile.js
@@ -3,12 +3,12 @@ var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('sass', function () {
-    return gulp.src('./src/scss/**/*.scss')
+    return gulp.src('../src/scss/**/*.scss')
       .pipe(sass().on('error', sass.logError))
       .pipe(autoprefixer())
-      .pipe(gulp.dest('./public/css'));
+      .pipe(gulp.dest('../public/css'));
 });
 
 gulp.task('default', function () {
-    gulp.watch('./src/scss/**/*.scss', gulp.series('sass'));
+    gulp.watch('../src/scss/**/*.scss', gulp.series('sass'));
 });
