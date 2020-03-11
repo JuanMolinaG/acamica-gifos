4cd3b62b31b555fda997b77ff4a5bbfad3fd040b
Juan G. Molina <jg.molina92@hotmail.com>
2020-03-11 15:33:27 -0500

    Add search gifs function and display none utility

--- public/js/main.js
+++ public/js/main.js
@@ -3,15 +3,15 @@ import ApiRequests from './api.js';
 
 // Endpoint URLs
 let trendsUrl = 'https://api.giphy.com/v1/gifs/trending?rating=g';
-let searchTermUrl = 'https://api.giphy.com/v1/gifs/search?q=asd&limit=20&offset=0&rating=G&lang=en';
+let searchTermUrl = 'https://api.giphy.com/v1/gifs/search?rating=G&lang=en';
 let searchIdUrl = 'https://api.giphy.com/v1/gifs/';
 
 // Variables
-let themeChangerButton = document.getElementsByClassName( 'theme-changer' )[0];
-let themeOptions = [...document.getElementsByClassName( 'theme-options' )[0].children];
-let searchInput = document.getElementsByClassName( 'search-input' )[0];
-let searchButton = document.getElementsByClassName( 'search-button' )[0];
-let suggestedSearchsContainer = document.getElementsByClassName( 'suggested-searchs' )[0];
+let themeChangerButton = document.querySelector( '.theme-changer' );
+let themeOptions = [ ...document.querySelector( '.theme-options' ).children ];
+let searchInput = document.querySelector( '.search-input' );
+let searchButton = document.querySelector( '.search-button' );
+let suggestedSearchsContainer = document.querySelector( '.suggested-searchs' );
 let apiRequest = new ApiRequests();
 
 // Event listeners
@@ -20,35 +20,39 @@ document.addEventListener( 'DOMContentLoaded', () => {
     setTrendGifs();
 });
 themeChangerButton.addEventListener( 'click', () => {
-    themeChangerButton.classList.toggle('active');
+    themeChangerButton.classList.toggle( 'active' );
 });
-themeOptions.forEach((option, index) => {
+themeOptions.forEach( ( option, index ) => {
     option.addEventListener( 'click', () => {
-        let body = document.getElementsByTagName('body')[0];
+        let body = document.querySelector( 'body' );
         if ( index == 0 ) {
-            body.classList.replace('sailor-night', 'sailor-day');
-            themeOptions[1].classList.remove('active');
+            body.classList.replace( 'sailor-night', 'sailor-day' );
+            themeOptions[1].classList.remove( 'active' );
         } else {
-            body.classList.replace('sailor-day', 'sailor-night');
-            themeOptions[0].classList.remove('active');
+            body.classList.replace( 'sailor-day', 'sailor-night' );
+            themeOptions[0].classList.remove( 'active' );
         }
-        option.classList.add('active');
+        option.classList.add( 'active' );
     })
-})
+});
 searchInput.addEventListener( 'keyup', (e) => {
     if ( e.target.value.length > 2 ) {
         let suggestedTerms = getSuggestedSearchs();
-        let suggestedButtons = [...document.getElementsByClassName('btn-suggested')];
+        let suggestedButtons = [ ...document.getElementsByClassName( 'btn-suggested' ) ];
 
-        suggestedButtons.forEach((button, index) => {
+        suggestedButtons.forEach( ( button, index ) => {
             button.innerHTML = suggestedTerms[index];
         });
-        suggestedSearchsContainer.classList.add('active');
-        searchButton.classList.replace('btn-disabled', 'btn-primary');
+        suggestedSearchsContainer.classList.add( 'active' );
+        searchButton.classList.replace( 'btn-disabled', 'btn-primary' );
     } else {
-        suggestedSearchsContainer.classList.remove('active');
-        searchButton.classList.replace('btn-primary', 'btn-disabled');
+        suggestedSearchsContainer.classList.remove( 'active' );
+        searchButton.classList.replace( 'btn-primary', 'btn-disabled' );
     }
+});
+searchButton.addEventListener( 'click', (e) => {
+    e.preventDefault();
+    setSearchGifs( searchInput.value );
 })
 
 //Functions
@@ -78,46 +82,70 @@ function getSuggestedSearchs() {
 
     let suggestedTerms = [];
 
-    for (let i = 0; i < 3; i++) {
-       let randomTerm = searchTerms[Math.floor(Math.random()*searchTerms.length)];
-       suggestedTerms.push(randomTerm);
-       searchTerms.splice(searchTerms.indexOf(randomTerm), 1);
+    for ( let i = 0; i < 3; i++ ) {
+       let randomTerm = searchTerms[ Math.floor( Math.random() * searchTerms.length ) ];
+       suggestedTerms.push( randomTerm );
+       searchTerms.splice( searchTerms.indexOf( randomTerm ), 1 );
     }
 
     return suggestedTerms;
 }
 
 async function setSuggestedGifs() {
-    let suggestedGifs = await apiRequest.fetchGet(trendsUrl, 4);
+    let suggestedGifs = await apiRequest.fetchGet( trendsUrl, 4 );
 
     if ( suggestedGifs ) {
         if ( suggestedGifs.data.length > 0 ) {
-            let suggestedItems = [...document.getElementsByClassName('suggested-item')];
-            suggestedItems.forEach((item, index) => {
-                const {title, images:{downsized_large:{url}}} = suggestedGifs.data[index];
+            let suggestedItems = [ ...document.getElementsByClassName( 'suggested-item' ) ];
+            suggestedItems.forEach( ( item, index ) => {
+                const { title, images:{ downsized_large:{ url } } } = suggestedGifs.data[index];
                 let html = `<div class="title-bar">
-                                <span>#${title}</span>
+                                <span>#${ title }</span>
                                 <img src="./assets/img/close.svg" alt="">
                             </div>
                             <div class="item-body">
-                                <img src="${url}" alt="${title}">
+                                <img src="${ url }" alt="${ title }">
                                 <a href="#" class="btn-sm btn-secondary">Ver más...</a>
                             </div>`;
                 item.innerHTML = html;
-                item.classList.remove('loading');
+                item.classList.remove( 'loading' );
             })
         }
     }
 }
 
 async function setTrendGifs() {
-    let trendGifs = await apiRequest.fetchGet(trendsUrl, 20, 4);
+    let trendGifs = await apiRequest.fetchGet( trendsUrl, 20, 4 );
 
     if ( trendGifs ) {
         if ( trendGifs.data.length > 0 ) {
-            let trendsContainer = document.getElementsByClassName('trends-container')[0];
+            let trendsContainer = document.querySelector( '.trends-container' );
             let html = '';
             trendGifs.data.forEach(gif => {
+                const { title, images:{ downsized_large:{ url } } } = gif;
+                html += `<div class="trend-item">
+                            <img src="${ url }" alt="${ title }">
+                            <div class="title-bar">
+                                <span>#${ title }</span>
+                            </div>
+                        </div>`
+            });
+            trendsContainer.innerHTML = html;
+        }
+    }
+}
+
+async function setSearchGifs( searchTerm ) {
+    let completeSearchUrl = `${ searchTermUrl }&q=${ searchTerm }`;
+    let resultGifs = await apiRequest.fetchGet( completeSearchUrl, 20 );
+    let trendsTitle = document.querySelector( '.trends-section .title-input' );
+    let suggestedSection = document.querySelector( '.suggested-section ' );
+    
+    if ( resultGifs ) {
+        if ( resultGifs.data.length > 0 ) {
+            let trendsContainer = document.getElementsByClassName('trends-container')[0];
+            let html = '';
+            resultGifs.data.forEach(gif => {
                 const {title, images:{downsized_large:{url}}} = gif;
                 html += `<div class="trend-item">
                             <img src="${url}" alt="${title}">
@@ -129,5 +157,8 @@ async function setTrendGifs() {
             trendsContainer.innerHTML = html;
         }
     }
+    suggestedSection.classList.add( 'd-none' );
+    trendsTitle.value = `${ searchTerm } (resultados)`;
+    suggestedSearchsContainer.classList.remove( 'active' );
 }
 
